5df6c389c68721241cb84fc3cbfb293ccf9094ff
Jun Kimura <jun.kimura@datachain.jp>
2021-08-03 18:58:46 +0900

    introduce cross-go and test is passed
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- go.mod
+++ go.mod
@@ -4,9 +4,13 @@ go 1.16
 
 require (
 	github.com/avast/retry-go v3.0.0+incompatible
-	github.com/btcsuite/btcd v0.20.1-beta
+	github.com/btcsuite/btcd v0.21.0-beta
 	github.com/btcsuite/btcutil v1.0.2
+	github.com/datachainlab/cross v0.1.1-0.20210803025654-71ed96d5ea91
 	github.com/ethereum/go-ethereum v1.9.25
-	github.com/stretchr/testify v1.4.0
+	github.com/gogo/protobuf v1.3.3
+	github.com/stretchr/testify v1.7.0
 	github.com/tyler-smith/go-bip39 v1.0.1-0.20181017060643-dbb3b84ba2ef
 )
+
+replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.2-alpha.regen.4
--- pkg/testing/cross_test.go
+++ pkg/testing/cross_test.go
@@ -2,11 +2,18 @@ package testing
 
 import (
 	"context"
+	"fmt"
 	"testing"
+
+	simpletypes "github.com/datachainlab/cross/x/core/atomic/protocol/simple/types"
+	"github.com/datachainlab/cross/x/core/tx/types"
+	xcctypes "github.com/datachainlab/cross/x/core/xcc/types"
+	"github.com/datachainlab/cross/x/packets"
+	"github.com/gogo/protobuf/proto"
+	"github.com/stretchr/testify/suite"
 
 	"github.com/datachainlab/cross-solidity/pkg/consts"
 	"github.com/datachainlab/cross-solidity/pkg/contract/crosssimplemodule"
-	"github.com/stretchr/testify/suite"
 )
 
 const testMnemonicPhrase = "math razor capable expose worth grape metal sunset metal sudden usage scheme"
@@ -24,7 +31,17 @@ func (suite *CrossTestSuite) SetupTest() {
 
 func (suite *CrossTestSuite) TestRecvPacket() {
 	ctx := context.Background()
-	packetData := []byte{}
+
+	txID := []byte(fmt.Sprintf("txid-%v", 0))
+
+	channel := xcctypes.ChannelInfo{}
+	xcc, err := xcctypes.PackCrossChainChannel(&channel)
+	suite.Require().NoError(err)
+
+	pdc := simpletypes.NewPacketDataCall(txID, types.NewResolvedContractTransaction(xcc, nil, types.ContractCallInfo{}, nil, nil))
+	pd := packets.NewPacketData(nil, pdc)
+	packetData, err := proto.Marshal(&pd)
+	suite.Require().NoError(err)
 
 	suite.Require().NoError(suite.chain.TxSyncIfNoError(ctx)(
 		suite.chain.CrossSimpleModule.OnRecvPacket(
