82c3437e541a3c8d47f07c45dd65557a8d328aae
YukiTsuchida <yuki.tsuchida@datachain.jp>
2025-10-27 15:21:11 +0900

    fix: move error declarations for better visibility in SimpleContractRegistry contract

--- src/core/SimpleContractRegistry.sol
+++ src/core/SimpleContractRegistry.sol
@@ -5,13 +5,13 @@ import "./ContractRegistry.sol";
 import "./IContractModule.sol";
 import {Packet} from "@hyperledger-labs/yui-ibc-solidity/contracts/core/04-channel/IIBCChannel.sol";
 
-error ModuleAlreadyInitialized();
-error ModuleNotInitialized();
-
 // SimpleContractRegistry is a simple registry that implements ContractRegistry
 abstract contract SimpleContractRegistry is ContractRegistry {
     // it keeps only one module.
     IContractModule internal contractModule;
+
+    error ModuleAlreadyInitialized();
+    error ModuleNotInitialized();
 
     function registerModule(IContractModule module) internal virtual override {
         if (address(contractModule) != address(0)) revert ModuleAlreadyInitialized();
