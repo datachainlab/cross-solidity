e4eac148b9e86a448d6b0232eddbd66909c0d90c
YukiTsuchida <yuki.tsuchida@datachain.jp>
2025-11-06 10:17:43 +0900

    refactor: remove unused imports of IInitiator and IAuthenticator in ICoordinator interface

--- src/core/ICoordinator.sol
+++ src/core/ICoordinator.sol
@@ -6,9 +6,6 @@ import {
     QueryCoordinatorStateResponse
 } from "src/proto/cross/core/atomic/simple/AtomicSimple.sol";
 
-import {IInitiator} from "./IInitiator.sol";
-import {IAuthenticator} from "./IAuthenticator.sol";
-
 interface ICoordinator {
     function coordinatorState(QueryCoordinatorStateRequest.Data calldata req)
         external
