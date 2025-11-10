7890f97f44c10bc1ea3680408e60ce93c4e8b7c9
YukiTsuchida <yuki.tsuchida@datachain.jp>
2025-11-10 09:17:57 +0900

    refactor: update _runTx to include a ToDo for transaction execution logic

--- src/core/TxRunner.sol
+++ src/core/TxRunner.sol
@@ -2,9 +2,10 @@ pragma solidity ^0.8.20;
 
 import {TxRunnerBase} from "./TxRunnerBase.sol";
 import {MsgInitiateTx} from "../proto/cross/core/initiator/Initiator.sol";
+import {CoreStore} from "./CoreStore.sol";
 
 abstract contract TxRunner is TxRunnerBase {
     function _runTx(bytes32, MsgInitiateTx.Data storage) internal virtual override {
-        // no-op
+        // ToDo: implement transaction execution logic
     }
 }
